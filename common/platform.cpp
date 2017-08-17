39ad2492d0ec9febd7349e3190ae9bbd5ae79700
Altay <altay_bn@yahoo.com>
2017-08-17 19:03:47 +0300

    First commit
    Empty project

---
+++ common/platform.cpp
@@ -0,0 +1,5 @@
+#include "platform.h"
+
+Platform::Platform() :
+{
+}
---
+++ common/platform.h
@@ -0,0 +1,14 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+#include <QWidget>
+
+class Platform
+{
+public:
+    Platform();
+
+private:
+};
+#endif // PLATFORM_H
+
+
