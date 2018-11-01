26e1fba22f371427dd1262ba8b9aec09abeef63d
altaybrusan <altay_bn@yahoo.com>
2018-11-01 17:30:46 +0300

    Submit to study table and instance table is completed in newpatientmgr

--- core/Controller/newpatientmgr.cpp
+++ core/Controller/newpatientmgr.cpp
@@ -41,64 +41,67 @@ void NewPatientMgr::OnRegistrationFormCompleted()
       _databaseMgr->OpenDatabase();
       if(!_databaseMgr->isConnectionOpen())
       {
-          LogMgr::instance()->LogSysFail("Can not open connection to database: NewPatientMgr");
+          LogMgr::instance()->LogSysFail("can not open connection to database: NewPatientMgr");
           return;
       }
+
       _databaseMgr->FetchDataFromDatabase();
-      QSqlRecord _record = _databaseMgr->GetRecordTemplateForStudyTable();
+      QString StudyId = DicomTools::GenerateStudyId();
+      for(int row = 0 ; row < m_model.GetAnatomicRegionList().count() ; row++)
+      {
+          QSqlRecord _record = _databaseMgr->GetRecordTemplateForStudyTable();
+          for(int a=0;a<=_record.count();a++)
+          {
+            _record.setValue(a,QString::number(0));
+          }
+          char uid[100];
+          _record.setValue("studyid",StudyId);
+          QMetaEnum _metaView = QMetaEnum::fromType<iBEX::PATIENT_POSISTION>();
+          int _indx = static_cast<int>(m_model.GetAnatomicRegionList().at(row).GetBodyPartView());
+          QString _view = QString::fromUtf8(_metaView.valueToKey(_indx));
+          _record.setValue("viewid",_view);
+          QMetaEnum _metaBodyPart = QMetaEnum::fromType<iBEX::BODY_PART>();
+          _indx = static_cast<int>(m_model.GetAnatomicRegionList().at(row).GetBodyPart());
+          QString _bodyPart = QString::fromUtf8(_metaBodyPart.valueToKey(_indx));
+          _record.setValue("bodypart",_bodyPart);
+          _record.setValue("seriesinstanceuid",dcmGenerateUniqueIdentifier(uid, SITE_SERIES_UID_ROOT));
+          _record.setValue("seriesstartdatetime",QDateTime::currentDateTime().date().toString("yyyy.MM.dd"));
+          QString sop =dcmGenerateUniqueIdentifier(uid, SITE_INSTANCE_UID_ROOT);
+          _record.setValue("primarysopuid",sop);
+          _record.setValue("secondarysopuid",sop+".1");
+          _record.setValue("imageacqdatetime",QDateTime::currentDateTime().time().toString());
+          _record.setValue("imagesendstatus","SCHEDULED");
+          _record.setValue("imageprintstatus","SCHEDULED");
+          _databaseMgr->AppendIntoStudyTable(_record);
+     }
+
+      char uid[100];
+      QSqlRecord _record = _databaseMgr->GetRecordTemplateForInstanceTable();
+      // initialize the record with dummy data
       for(int a=0;a<=_record.count();a++)
       {
-        _record.setValue(a,QString::number(0));
+          _record.setValue(a,QString::number(0));
       }
-      char uid[100];
-      QString StudyId = DicomTools::GenerateStudyId();
       _record.setValue("studyid",StudyId);
-      QMetaEnum _metaView = QMetaEnum::fromType<iBEX::PATIENT_POSISTION>();
-      int _indx = static_cast<int>(m_model.GetAnatomicRegionList().at(0).GetBodyPartView());
-      QString _view = QString::fromUtf8(_metaView.valueToKey(_indx));
-      _record.setValue("viewid",_view);
-      _record.setValue("seriesinstanceuid",dcmGenerateUniqueIdentifier(uid, SITE_SERIES_UID_ROOT));
-      _record.setValue("seriesstartdatetime",QDateTime::currentDateTime().date().toString("yyyy.MM.dd"));
-      QString sop =dcmGenerateUniqueIdentifier(uid, SITE_INSTANCE_UID_ROOT);
-      _record.setValue("primarysopuid",sop);
-      _record.setValue("secondarysopuid",sop+".1");
-      _record.setValue("imageacqdatetime",QDateTime::currentDateTime().time().toString());
-      _record.setValue("imagesendstatus","SCHEDULED");
-      _record.setValue("imageprintstatus","SCHEDULED");
-      _databaseMgr->AppendIntoStudyTable(_record);
+      _record.setValue("accessionnum",m_model.GetAccessionNumber());
+      QString _fullName;
+      if(!m_model.GetPatientMiddleName().isEmpty())
+          _fullName = _fullName + "^"+ m_model.GetPatientMiddleName();
+      if(!m_model.GetPatientLastName().isEmpty())
+          _fullName = _fullName + "^" + m_model.GetPatientLastName();
+      _record.setValue("patientname",_fullName);
+      _record.setValue("patientid",m_model.GetPatientId());
+      _record.setValue("sex",m_model.GetPatientGender());
+      _record.setValue("refphysician",m_model.GetReferringPhysician());
+      _record.setValue("admittingtime",QDateTime::currentDateTime().date().toString(Qt::ISODate));
+      _record.setValue("studyinstanceuid",dcmGenerateUniqueIdentifier(uid, SITE_SERIES_UID_ROOT));
+      _record.setValue("visitcomment","LOCAL");
+      _record.setValue("studystatus","SCHEDULED");
+      _record.setValue("studystartdatetime",QDate::currentDate().toString(Qt::ISODate));
+      //OTHER FIELDS ARE NOT INSERTED YET
+      _databaseMgr->AppendIntoInstanceTable(_record);
 
-     for(int row = 0 ; row < m_model.GetAnatomicRegionList().count() ; row++)
-      {
-          QSqlRecord _record = _databaseMgr->GetRecordTemplateForInstanceTable();
 
-          LogMgr::instance()->LogSysInfo("Fields count: "+ QString::number(_record.count()));
-//          // initialize the record with dummy data
-//          for(int a=0;a<=_record.count();a++)
-//          {
-//              _record.setValue(a,QString::number(0));
-//          }
-//          _record.setValue("studyid",StudyId);
-//          _record.setValue("accessionnum",m_model.GetAccessionNumber());
-
-//          QString _fullName;
-
-//          if(!m_model.GetPatientMiddleName().isEmpty())
-//              _fullName = _fullName + "^"+ m_model.GetPatientMiddleName();
-//          if(!m_model.GetPatientLastName().isEmpty())
-//              _fullName = _fullName + "^" + m_model.GetPatientLastName();
-//          _record.setValue("patientname",_fullName);
-//          _record.setValue("patientid",m_model.GetPatientId());
-//          _record.setValue("sex",m_model.GetPatientGender());
-//          _record.setValue("refphysician",m_model.GetReferringPhysician());
-//          _record.setValue("admittingtime",QDateTime::currentDateTime().date().toString(Qt::ISODate));
-//          _record.setValue("studyinstanceuid",dcmGenerateUniqueIdentifier(uid, SITE_SERIES_UID_ROOT));
-//          _record.setValue("visitcomment","LOCAL");
-//          _record.setValue("studystatus","SCHEDULED");
-//          _record.setValue("studystartdatetime",QDate::currentDate().toString(Qt::ISODate));
-//          //OTHER FIELDS ARE NOT INSERTED YET
-//          _databaseMgr->AppendIntoInstanceTable(_record);
-      }
-
-          m_examinationMgr.OnActivateExaminationAfterRegistration(m_model);
+      m_examinationMgr.OnActivateExaminationAfterRegistration(m_model);
 
 }
